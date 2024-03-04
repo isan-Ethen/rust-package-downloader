6af4b31af54b5a3661fa5cb0c83a923040c6caab
大松 生輝 <m21029@g.metro-cit.ac.jp>
2024-03-03 20:17:29 -0800

    Revise installation method to support multi-threading instead of single-threading.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use bytes::Bytes;
 use error_chain::error_chain;
+use once_cell::sync::Lazy;
+use rust_package_downloader::ThreadPool;
 use std::fs;
 use std::fs::File;
 use std::io::copy;
+use std::sync::atomic::{
+    AtomicUsize,
+    Ordering::{Acquire, Release},
+};
+
+const LIST_FILE_PATH: &str = "list.txt";
+const DIR_PATH: &str = "packages/";
+const POOL_SIZE: usize = 8;
+static NUMBER_OF_THREAD: AtomicUsize = AtomicUsize::new(0);
+static CONTENTS: Lazy<String> = Lazy::new(|| {
+    let contents = fs::read_to_string(LIST_FILE_PATH).expect("Couldn't read the file");
+    contents
+});
 
 error_chain! {
     foreign_links {
@@ -12,26 +28,43 @@ error_chain! {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let file_path = "list.txt";
-    let dir_path = "packages/";
+    let pool = ThreadPool::new(POOL_SIZE);
+    fs::create_dir_all(DIR_PATH)?;
 
-    fs::create_dir_all(dir_path)?;
+    let rows: Vec<&str> = CONTENTS.split("\n").collect::<Vec<&str>>();
+    let len = rows.len();
 
-    let urls = fs::read_to_string(file_path).expect("cannnot read the file");
-    let mut https: Vec<&str> = urls.split("\n").collect();
-
-    https.pop();
-    for package in https {
-        let package_info = package.split_whitespace().collect::<Vec<&str>>();
-        let url = package_info[0];
-        let fname = package_info[1];
-        let mut dest = File::create(format!("{}/{}", dir_path, fname))?;
-        let response = reqwest::get(&url[1..url.len() - 1]).await?;
-        let content = response.bytes().await?;
-        copy(&mut content.as_ref(), &mut dest)?;
+    for row in rows[..len - 1].iter() {
+        let package_info = row.split_whitespace().collect::<Vec<&str>>();
+        pool.execute(move || {
+            count_up();
+            let url = package_info[0];
+            let filename = package_info[1];
+            let mut dest = File::create(format!("{}/{}", DIR_PATH, filename))
+                .unwrap_or_else(|why| panic!("Couldn't creat file {}: {}", filename, why));
+            async {
+                let response = reqwest::get(&url[1..url.len() - 1]).await;
+                let content: Bytes = response
+                    .expect("There no Response!")
+                    .bytes()
+                    .await
+                    .expect("Couldn't cast content to string");
+                copy(&mut content.as_ref(), &mut dest).expect("Couldn't write content into {}");
+            };
+            count_down();
+        });
     }
+
+    while NUMBER_OF_THREAD.load(Acquire) != 0 {}
 
     println!("All files downloaded successfully!");
 
     Ok(())
+}
+fn count_up() {
+    NUMBER_OF_THREAD.fetch_add(1, Release);
+}
+
+fn count_down() {
+    NUMBER_OF_THREAD.fetch_sub(1, Release);
 }
